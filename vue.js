2785e53a939ec273b3627ff36baa809774fd9e8b
yesthisiskendra <yesthisiskendra@gmail.com>
2019-06-29 11:03:35 -0700

    Add place to view submitted reviews

--- vue.js
+++ vue.js
@@ -102,6 +102,17 @@ Vue.component("kitten-component", {
                 :class="{ disabledButton: !adoptable }"> Remove from Basket 
             </button>
             
+            <div>
+            <h2> Reviews </h2>
+            <p v-if="!reviews.length">No reviews yet</p>
+            <ul>
+                <li v-for="review in reviews">
+                    <p>{{ review.name }}</p>
+                    <p>Review: {{ review.review }}</p>
+                    <p>Rating: {{ review.rating }}</p>
+                </li>
+            </ul>
+            </div>
             <kitten-review @review-submitted="addReview"></kitten-review>
 
         </div>
