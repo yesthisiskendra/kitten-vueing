a5d9a6a7aa9e2ad49a5b9c01a288666a1f9b9edb
yesthisiskendra <yesthisiskendra@gmail.com>
2019-06-29 11:03:34 -0700

    Adding kitten-component

--- vue.js
+++ vue.js
@@ -1,5 +1,8 @@
 //Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.
 
+Vue.component("kitten-component", {
+  template: `<h1>I AM A KITTEN!</h1>`
+});
 var app = new Vue({
   el: "#app",
   data: {
